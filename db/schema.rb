48c640f16e7f04187f379dc9166154232d8a9481
rotimiazeez <harzeyzhrotimi@gmail.com>
2022-09-15 22:24:07 +0100

    Fixes #2 - Create job controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_145408) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_210517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.string "job_type"
+    t.string "location"
+    t.string "job_author"
+    t.boolean "remote_ok"
+    t.string "apply_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avatar"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
